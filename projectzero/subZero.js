dea8f24783237c18864687d3a378ef7a85acbfcb
patrickliamhartley <patrickliamhartley@gmail.com>
2016-08-02 12:13:19 -0700

    Implement projectzero character before refactoring

--- projectzero/subZero.js
+++ projectzero/subZero.js
@@ -1,21 +1,96 @@
 // Creates and returns a new dancer object that can step
 var subZero = function() {
 
-  var subZero = '<img class="sub" src=projectzero/standing.gif>'; 
+  var subZero = '<img id="sub" src=projectzero/standing.gif>'; 
   $("a").empty();
   $('body').css('background-image', 'url(projectzero/back.png)');
   $('body').append(subZero);
-  $(document).keyup(function(evt) {
-    if (evt.keyCode == 32) {
-      
-      subZero='<img class="sub" src=projectzero/walking.gif>';
+  var title = '<img id="fight" src=projectzero/fight.gif>';
+  $('body').append(title);
+  $('#fight').fadeOut(1400);
 
+  var standing = false;
+  var attacking = false;
+
+  //to set standing and walking gifs
+  var standF = function () {
+    if (standing === true) {
+      $('#sub').attr("src", 'projectzero/walking.gif');
+    } else {
+      $('#sub').attr("src", 'projectzero/standing.gif');
     }
+  };
+
+  //attack function
+  var attack = function() {
+    if (attacking === true) {
+      $('#sub').attr("src",'projectzero/sAttack.gif');
+    } else {
+      $('#sub').attr("src",'projectzero/standing.gif');
+    }
+
+  };
+  
+
+  $(document).keydown(function(evt) {
+    console.log(evt.keyCode);
+    
+//directional movement
+    if (evt.keyCode === 40) {     
+      $('#sub').css({position:'relative','top':"+=10px"});
+      standing = true;
+      standF();
+    }
+    if (evt.keyCode === 38) {     
+      $('#sub').css({position:'relative','top':"-=10px"});
+      standing = true;
+      standF();
+    }
+    if (evt.keyCode === 39) {     
+      $('#sub').css({position:'relative','right':"-=10px"});
+      standing = true;
+      standF();
+    }
+    if (evt.keyCode === 37) {     
+      $('#sub').css({position:'relative','right':"+=10px"});
+      standing = true;
+      standF();
+    }
+//special attack
+    if (evt.keyCode === 93) {     
+      attacking = true;
+      attack();
+    }    
+
   });
 
-  
-  // $('body').append(subZero.$back);
-};  
+  $(document).keyup(function(e) {
+  //keyup to turn off walking animations
+    console.log(e.keyCode);
+    if (e.keyCode === 39) {
+      standing = false;
+      standF();
+    }
+    if (e.keyCode === 37) {
+      standing = false;
+      standF();
+    }
+    if (e.keyCode === 38) {
+      standing = false;
+      standF();
+    }
+    if (e.keyCode === 40) {
+      standing = false;
+      standF();
+    }
+//turn off attacking animation
+    if (e.keyCode === 93) {
+      attacking = false;
+      attack();
+    }
+
+  });
+};    
 //   this.$node = $('<span class="fighter"></span>');
 //   this.setPosition(this.top, this.left);
 //   //initates step function to start repeating
